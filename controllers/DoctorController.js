d7877ec4f0c304aa468c7aa962270a0695be97f6
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-02-10 17:50:27 +0100

    Added query to controllers

--- controllers/DoctorController.js
+++ controllers/DoctorController.js
@@ -6,7 +6,8 @@ function index(req, res) {
 
     const sql = ` SELECT  doctors.*, AVG(reviews.vote) AS "vote_avarage" FROM doctors
                   JOIN reviews ON doctors.id = reviews.id_doctor
-                  GROUP BY doctors.id `
+                  GROUP BY doctors.id 
+                  ORDER BY vote_avarage DESC`
 
     connection_db.query(sql, (err, results) => {
 
@@ -53,12 +54,31 @@ function show(req, res) {
 }
 
 
-
 function store(req, res) {
     res.send('sono post')
 }
+
+
 function destroy(req, res) {
-    res.send('sono delete')
+
+    const id = req.params.id
+    const sqlSingleDoctor = `DELETE FROM doctors WHERE id = ?`
+
+    connection_db.query(sqlSingleDoctor, [id], (err, results) => {
+
+        if (err) { return res.status(500).json({ error: 'Internal error server' }) }
+
+        if (results.effectedRows !== 1) {
+            return res.status(400).json({ error: 'Is not possible delete this doctor, beacause he does not exist' })
+        }
+
+        res.json(
+            {
+                doctor: results,
+            }
+        )
+
+    })
 }
 
 export { index, show, store, destroy }
