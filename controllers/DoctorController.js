eeca3d1d8a478033f04a8b1d01ec3cb07cafda8b
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2025-02-11 18:22:29 +0100

    changes

--- controllers/DoctorController.js
+++ controllers/DoctorController.js
@@ -8,18 +8,18 @@ function index(req, res) {
     doctors.*, 
     GROUP_CONCAT(DISTINCT specializations.name ORDER BY specializations.name SEPARATOR ', ') AS specializations,
     AVG(reviews.vote) AS vote_average 
-FROM doctors
-JOIN reviews ON doctors.id = reviews.id_doctor
-JOIN doc_spec ON doctors.id = doc_spec.id_doctor
-JOIN specializations ON specializations.id = doc_spec.id_specialization
-GROUP BY doctors.id 
-ORDER BY vote_average DESC;`
+    FROM doctors
+    JOIN reviews ON doctors.id = reviews.id_doctor
+    JOIN doc_spec ON doctors.id = doc_spec.id_doctor
+    JOIN specializations ON specializations.id = doc_spec.id_specialization
+    GROUP BY doctors.id 
+    ORDER BY vote_average DESC;`
 
 
     connection_db.query(sql, (err, results) => {
 
+        // 
         if (err) { return res.status(500).json({ error: 'Internal error server' }) }
-
         if (results.length === 0) { return res.status(200).json({ message: 'No doctors Available' }) }
 
         return res.json(
@@ -45,25 +45,22 @@ function show(req, res) {
     HAVING doctors.id = ?`
 
     connection_db.query(sqlSingleDoctor, [id], (err, results) => {
-        //Se c'è un errore ritorna un error 500
-        if (err) { return res.status(500).json({ error: 'Internal error server' }) }
-        //Se l'elemento non è presente ritorna un error 404
-        if (!results[0]) return res.status(404).json({ error: "Doctor not found" });
+
+        if (err) { return res.status(500).json({ error: 'Internal error server' }) } //Se c'è un errore ritorna un error 500
+        if (!results[0]) return res.status(404).json({ error: "Doctor not found" }); //Se l'elemento non è presente ritorna un error 404
+
         //Altrimenti ritorna l'elemento ma prima dobbiamo estrarre le sue reviews
         if (results[0]) {
-            //preparo la query
+
             const sqlDoctorReviews = `SELECT * FROM reviews
-            WHERE id_doctor = ?`;
-            //salvo il dottore nella variabile doctor
-            const doctor = results[0];
-            //Esecuzione query
+            WHERE id_doctor = ?`; //preparo la query
+            const doctor = results[0]; //salvo il dottore nella variabile doctor
+
+            // Esecuzione query per le reviews
             connection_db.query(sqlDoctorReviews, [id], (err, results2) => {
-                //Se c'è un errore ritorna un error 500
-                if (err) return res.status(500).json({ error: err });
-                //Salvo le reviews nella variabile reviews
-                doctor.reviews = results2;
-                //ritorno l'elemento
-                return res.json(doctor);
+                if (err) return res.status(500).json({ error: err }); //Se c'è un errore ritorna un error 500
+                doctor.reviews = results2;  //Salvo le reviews nella variabile reviews
+                return res.json(doctor); //Ritorno l'elemento con le reviews aggiunte
             });
         };
     });
@@ -84,7 +81,6 @@ function destroy(req, res) {
     connection_db.query(sqlSingleDoctor, [id], (err, results) => {
 
         if (err) { return res.status(500).json({ error: 'Internal error server' }) }
-
         if (results.effectedRows !== 1) {
             return res.status(400).json({ error: 'Is not possible delete this doctor, beacause he does not exist' })
         }
